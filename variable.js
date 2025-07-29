c98e6a01fecf923927af8a35af45467f4c819b09
epon roy <eponroy16@gmail.com>
2025-07-29 12:22:47 +0600

    added variable.js file

---
+++ variable.js
@@ -0,0 +1,38 @@
+/*
+ *VARIABLE ==> CONTAINER
+ */
+
+// number
+var sunglassPrice = 80;
+
+// string
+
+var subject = "EEE";
+
+//boolean
+var passed = true;
+
+/*  
+*NAMING CONVENTION
+
+*single word
+*no quote
+*no gap or dash
+*can't start with number
+*can no be a keyword
+*prefer camelCase
+*
+*
+*/
+
+var lastExamREsult = 89;
+
+/*
+ *OPERATION
+ * + , - , * , / , %
+ * += , -= , *= , /=
+ * isNAN?
+ *null
+ *NUmber
+ *
+ */
