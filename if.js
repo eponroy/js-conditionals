990d0e0b7d2f608d908c54d670ef1c8108fefca3
epon roy <eponroy16@gmail.com>
2025-07-29 21:00:41 +0600

    added if.js file

---
+++ if.js
@@ -0,0 +1,37 @@
+/* 
+*if you buy food for me 
+*     i will come
+*
+*--------------------------------
+*if biriyani price less than 300 tk
+*      i will buy
+*
+*-------------STRUCTURE-------------------
+*if (condition){
+
+}
+
+*
+*/
+
+if (13 < 10) {
+  console.log("i have a value");
+} else {
+  console.log("murukha");
+}
+
+/* 
+if (condition){
+execute code if condition is true 
+}
+else{
+    execute code if condition is false
+    }
+*/
+
+var weight = 40;
+if (weight < 20) {
+  console.log("i will carry it by myself");
+} else {
+  console.log("i will rent a rickshawe");
+}
